65999bc89c2aca9cf80ad7ba7ba226e902b5a614
Glich404 <72074134+Glich404@users.noreply.github.com>
2021-02-21 11:57:33 +0100

    Ban Fix

--- commands/test/bankickmute.js
+++ commands/test/bankickmute.js
@@ -6,11 +6,51 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-// Ban a user by ID (or with a user/guild member object)
-if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need the Ban Members permission.")
-if (!message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])) return message.channel.send("You need to provide a valid user.")
-
-guild.members.ban(message.mentions.users.first().id || message.guild.members.get(args[0])
-  .then(user => console.log(`Banned ${user.username || user.id || user} from ${guild.name}`))
-  .catch(console.error));
-  }}
+    client.on('message', message => {
+      // Ignore messages that aren't from a guild
+      if (!message.guild) return;
+    
+      // if the message content starts with "!ban"
+      if (!message.content.startsWith('!ban')) {
+        // Assuming we mention someone in the message, this will return the user
+        const user = message.mentions.users.first();
+        // If we have a user mentioned
+        if (user) {
+          // Now we get the member from the user
+          const member = message.guild.members.resolve(user);
+          // If the member is in the guild
+          if (member) {
+            /**
+             * Ban the member
+             * Make sure you run this on a member, not a user!
+             * There are big differences between a user and a member
+             * Read more about what ban options there are over at
+             * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+             */
+            member
+              .ban({
+                reason: 'They were bad!',
+              })
+              .then(() => {
+                // We let the message author know we were able to ban the person
+                message.channel.send(`Successfully banned ${user.tag}`);
+              })
+              .catch(err => {
+                // An error happened
+                // This is generally due to the bot not being able to ban the member,
+                // either due to missing permissions or role hierarchy
+                message.channel.send('I was unable to ban the member');
+                // Log the error
+                console.error(err);
+              });
+          } else {
+            // The mentioned user isn't in this guild
+            message.channel.send("That user isn't in this guild!");
+          }
+        } else {
+          // Otherwise, if no user was mentioned
+          message.channel.send("You didn't mention the user to ban!");
+        }
+      }
+    });
+  }}    
