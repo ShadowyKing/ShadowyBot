e5ca8cb08376b1ffb7a9a1026a5fd7ecc8fdfe58
Glich404 <72074134+Glich404@users.noreply.github.com>
2021-02-21 11:51:46 +0100

    Ban FIx v1

--- commands/test/bankickmute.js
+++ commands/test/bankickmute.js
@@ -6,15 +6,11 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
+// Ban a user by ID (or with a user/guild member object)
+if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need the Ban Members permission.")
+if (!message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])) return message.channel.send("You need to provide a valid user.")
 
-if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
-let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-if (!User) return message.channel.send("Invalid User")
-if (User.hasPermission("BAN_MEMBERS")) return message.reply("Invalid Permissions")
-let banReason = args.join(" ").slice(22);
-if (!banReason) {
-  banReason = "None"
-}
-
-User.ban({reason: banReason})
+guild.members.ban(message.mentions.users.first().id || message.guild.members.get(args[0])
+  .then(user => console.log(`Banned ${user.username || user.id || user} from ${guild.name}`))
+  .catch(console.error));
   }}
