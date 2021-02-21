f16adb2ab87d005040e445f6a1e572fb74d37165
Glich404 <72074134+Glich404@users.noreply.github.com>
2021-02-21 13:39:43 +0100

    Update code.js

--- commands/test/code.js
+++ commands/test/code.js
@@ -9,7 +9,7 @@ module.exports = class TestCommand extends BaseCommand {
   async run(client, message, args) {
       if (message.author = "TheShadowyDev#6519"){
         message.channel.send("Hello, the code is in your dms!")
-        message.author.send("The code is: 3846129462508362")
+        message.author.send("Test")
       }
    else {
     message.Channel.send("Dont try to trick me, you are not my creator!");
