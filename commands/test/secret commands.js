6d4eaa04d1188402939b42173f8a2650577c029b
Glich404 <72074134+Glich404@users.noreply.github.com>
2021-08-03 12:28:17 +0200

    The One

--- commands/test/secret commands.js
+++ commands/test/secret commands.js
@@ -3,13 +3,13 @@ const BaseCommand = require('../../utils/structures/BaseCommand');
 
 module.exports = class TestCommand extends BaseCommand {
   constructor() {
-    super('1311', '1311', []);
+    super('3846129462508362', '3846129462508362', []);
   }
 
   async run(client, message, args) {
-      message.reply("Congrats you found a secret command, please check your dms and send my creator the code for reward!")
+      message.reply("Great, check your dms!")
       message.delete()
-      message.author.send("Code is: 3846129462508362")
+      message.author.send("Roblox Id: 854449010 ")
 
 
 
